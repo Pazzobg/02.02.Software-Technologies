3206b0948a38b1569663bd30d0ad25b8996f0545
Plamen Stoyanov <pazzo_bg@yahoo.com>
2017-07-21 14:28:50 +0300

    Added files from Blog-Project

---
+++ 02.PHP/03.PHP-Blog-Basic-Functionality/SoftUniBlogBundle/Controller/ArticleController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace SoftUniBlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use SoftUniBlogBundle\Entity\Article;
+use SoftUniBlogBundle\Form\ArticleType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\HttpFoundation\Request;
+
+class ArticleController extends Controller
+{
+    /**
+     * @param Request $request
+     *
+     * @Route ("/article/create", name="article_create")
+     * @Security ("is_granted('IS_AUTHENTICATED_FULLY')")
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function create(Request $request)
+    {
+        $article = new Article();
+        $form = $this->createForm(ArticleType::class, $article);
+
+        $form->handleRequest($request);
+
+        if  ($form->isSubmitted() && $form->isValid()) {
+            $article->setAuthor($this->getUser());
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($article);
+            $em->flush();
+
+            return $this->redirectToRoute('blog_index');
+        }
+
+        return $this->render('article/create.html.twig',
+            array('form' => $form->createView()));
+    }
+}
