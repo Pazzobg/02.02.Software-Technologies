6dca30129387bdae47d61ba66f4da7de169ec421
Plamen Stoyanov <pazzo_bg@yahoo.com>
2017-08-06 07:33:04 +0300

    Added Calculator JavaScript

---
+++ 03.JavaScript/02.CalculatorJS/controllers/home.js
@@ -0,0 +1,31 @@
+const Calculator = require('../models/Calculator');
+
+module.exports = {
+    indexGet: (req, res) => {
+        res.render('home/index');
+    },
+    indexPost: (req, res) => {
+        let calculatorBody = req.body;
+
+        let calculatorParams = calculatorBody['calculator'];
+
+        let calculator = new Calculator();
+        calculator.leftOperand = Number(calculatorParams.leftOperand);
+        calculator.operator = calculatorParams.operator;
+        calculator.rightOperand = Number(calculatorParams.rightOperand);
+
+        /*Another way to initialize the calc-object:
+        let calculator = new Calculator(
+            Number(calculatorParams.leftOperand),
+            calculatorParams.operator,
+            Number(calculatorParams.rightOperand)
+        );*/
+
+        let result = calculator.calculateResult();
+
+        res.render('home/index', {
+            'calculator': calculator,
+            'result': result
+        });
+    }
+};
---
+++ 03.JavaScript/02.CalculatorJS/models/calculator.js
@@ -0,0 +1,56 @@
+function Calculator(leftOperand, operator, rightOperand) {
+    this.leftOperand = leftOperand;
+    this.operator = operator;
+    this.rightOperand = rightOperand;
+
+    this.calculateResult = function () {
+        let result = 0;
+
+        switch (this.operator) {
+            case 'AND':
+                result = this.leftOperand & this.rightOperand;
+                break;
+            case 'OR':
+                result = this.leftOperand | this.rightOperand;
+                break;
+            case 'XOR':
+                result = this.leftOperand ^ this.rightOperand;
+                break;
+            case '+':
+                result = this.leftOperand + this.rightOperand;
+                break;
+            case '-':
+                result = this.leftOperand - this.rightOperand;
+                break;
+            case '*':
+                result = this.leftOperand * this.rightOperand;
+                break;
+            case '/':
+                if (this.rightOperand !== 0){
+                    result = this.leftOperand / this.rightOperand;
+                    break;
+                } else {
+                    result = '!possible';
+                    break;
+                }
+            case '%':
+                if (this.rightOperand !== 0) {
+                    result = this.leftOperand % this.rightOperand;
+                    break;
+                } else {
+                    result = '!possible';
+                    break;
+                }
+            case '^':
+                result = this.leftOperand ** this.rightOperand;
+                break;
+            case '√':
+                result = Math.sqrt(this.leftOperand);
+                break;
+        }
+
+        return result;
+    }
+}
+
+module.exports = Calculator;
